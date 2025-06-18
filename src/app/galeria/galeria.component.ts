e0717f744f3dd7125d5bb364ab1f0a19abfc88b6
TheylorGR <N00266117@upn.pe>
2025-06-18 03:51:33 -0500

    carrusel interactivo para la galeria

--- src/app/galeria/galeria.component.ts
+++ src/app/galeria/galeria.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import Swiper from 'swiper';
+import { Autoplay } from 'swiper/modules';
 
 @Component({
   selector: 'app-galeria',
-  imports: [],
   templateUrl: './galeria.component.html',
-  styleUrl: './galeria.component.css'
+  styleUrls: ['./galeria.component.css']
 })
-export class GaleriaComponent {
+export class GaleriaComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    Swiper.use([Autoplay]);
+
+    const swiper = new Swiper('.galeriaSwiper', {
+      loop: true,
+      slidesPerView: 'auto', // usa ancho automático para deslizar suave
+      spaceBetween: 20,
+      speed: 4000, // velocidad de animación (ms)
+      freeMode: true, // activa modo libre
+      autoplay: {
+        delay: 0, // sin pausa entre movimientos
+        disableOnInteraction: false,
+      },
+      grabCursor: true,
+    });
+  }
 
 }
