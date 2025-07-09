b98cb8fe7efc43fa587d1362968204b7f4a4d1ac
JorgeRiveraMio <jorgeriveramio1008@gmail.com>
2025-07-09 13:48:20 -0500

    card accepts pago

--- src/app/pago/pago.component.ts
+++ src/app/pago/pago.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { UsuarioService } from '../../Servicios/Service/usuario.service';
 import { SesionService } from '../../Servicios/Service/sesion.service';
@@ -8,6 +8,7 @@ import { Router, RouterModule } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
 import { MercadoPagoService } from '../../Servicios/Service/mercado-pago.service';
 import { CommonModule } from '@angular/common';
+import { UsuarioActual } from '../../Modelos/Entity/UsuarioActual';
 
 @Component({
   selector: 'app-pago',
@@ -15,7 +16,7 @@ import { CommonModule } from '@angular/common';
   templateUrl: './pago.component.html',
   styleUrl: './pago.component.css'
 })
-export class PagoComponent {
+export class PagoComponent implements OnInit {
   isCollapsed = false;
     nombreUsuario: string = '';
     rolUsuario: string = '';
@@ -27,13 +28,29 @@ export class PagoComponent {
     tiposSesion: any[] = [];
     horasDisponibles: string[] = [];
     
-    
+   ngOnInit(): void {
+    const tomorrow = new Date();
+    tomorrow.setDate(tomorrow.getDate() + 1);
+    this.minDate = this.formatDateToInput(tomorrow);
+   
+
+    this.usuarioService.obtenerUsuarioActual().subscribe({
+      next: (usuario: UsuarioActual) => {
+        this.nombreUsuario = usuario.name || usuario.username || 'Usuario';
+        this.idUsuario = usuario.idUsuario;
+        this.rolUsuario = this.obtenerRol(usuario.idRol);
+        
+      },
+      error: () => {
+        this.nombreUsuario = 'Usuario';
+        this.rolUsuario = '';
+      }
+    });
+   
+  } 
   constructor(
     private fb: FormBuilder,
-    private usuarioService: UsuarioService,
-    private sesionService: SesionService,
-    private tipoSesionService: tipoSesionService,
-    private pacienteService: PacienteService,
+    private usuarioService: UsuarioService,   
     private router: Router,
     private toastr: ToastrService,
     private pagoService: MercadoPagoService // Asegúrate de importar el servicio de pago
@@ -47,5 +64,20 @@ export class PagoComponent {
     this.usuarioService.logout();
     this.router.navigate(['/']);
   }
+    obtenerRol(idRol: number): string {
+    switch (idRol) {
+      case 1: return 'ROLE_TUTOR';
+      case 2: return 'ROLE_ADMIN';
+      case 3: return 'ROLE_TERAPEUTA';
+      default: return '';
+    }
+  }
+   formatDateToInput(date: Date): string {
+    const year = date.getFullYear();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  }
+
 }
 
