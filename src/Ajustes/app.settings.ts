41f7f877d76f2066df37c22c827c2ce04b67710e
marco <marcofabianj@hotmail.com>
2025-07-09 11:22:05 -0500

    Refactor: usar appsettingsCliente y environment en servicios

--- src/Ajustes/app.settings.ts
+++ src/Ajustes/app.settings.ts
@@ -5,6 +5,6 @@ export const appsettingsCliente = {
 };
 
 export const appsettingsDni={
-    apiUrl:"http://localhost:8080/consultar-dni",
+    apiUrl: environment.apiUrl + "/consultar-dni",
     apiKey:"apis-token-10511.jrLt6-0vX9b2y3fABmj4J-yYAqR4Up9s"
 }
